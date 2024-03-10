6447dfb5ff4f1152520fa1bc03174e099bc53a0d
saffronjam <own.emka@gmail.com>
2024-03-10 16:44:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,5 @@ apt-get update && apt-get install -y python3 python3-pip
 # Install Ansible using Python pip
 pip3 install ansible
 
-# Clone the specified git repository
-cd /home/cloud
-git clone https://github.com/kthcloud/ansible-setup-host.git
-
-# Run the Ansible playbook
-cd /home/cloud/ansible-setup-host
+# Run the ansible playbook
 ansible-playbook setup-host.yml
