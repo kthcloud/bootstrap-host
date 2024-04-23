04b65a60507a651413ddb5bb3348431817e771eb
saffronjam <own.emka@gmail.com>
2024-04-23 10:24:58 +0200

    Update GPU passthrough script to use config

--- templates/print_passthrough_gpus.py
+++ templates/print_passthrough_gpus.py
@@ -3,7 +3,8 @@
 import subprocess
 import json
 import sys
-
+import os
+import yaml
 
 def output_empty():
     print(json.dumps({"bus_ids": "", "device_ids": "", "has_non_passthrough": False, "requires_configuration": False}))
@@ -17,53 +18,90 @@ def run_cmd(cmd):
     proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
     (res, _) = proc.communicate()
     proc.wait()
-
     return res
 
-gpu_cmd = "lspci -knnmmvvv | jc --lspci"
-res = run_cmd(gpu_cmd)
-if len(res) == 0:
-    output_empty()
+def load_config():
+    possible_places = [
+        "/etc/kthcloud/config.yml",
+        "/etc/kthcloud/config.yaml",
+        "./config.yml",
+        "./config.yaml"
+    ]
 
-devices = json.loads(res)
-bus_ids = []
-device_ids = set()
-# Run the gpu_cmd to find out what to skip
-skip_gpus = ["RTX A6000"]
+    config = None
+    for file in possible_places:
+        if os.path.exists(file):
+            with open(file, "r") as f:
+                config = yaml.safe_load(f)
+                break
 
-requires_configuration = False
-has_non_passthrough = False
+    if config is None:
+        raise ValueError("Failed to load config file")
 
-for device in devices:
-    skip = False
+    return config
 
-    if "driver" not in device:
-        continue
 
-    for skip_gpu in [item for item in skip_gpus if "device" in device]:
-        skip = skip_gpu.lower() in device["device"].lower()
+def main():
+    config = load_config()
+
+    gpu_cmd = "lspci -knnmmvvv | jc --lspci"
+    res = run_cmd(gpu_cmd)
+    if len(res) == 0:
+        output_empty()
+
+    devices = json.loads(res)
+    bus_ids = []
+    device_ids = set()
+
+    # Which GPUs that should not be passthrough
+    # Any name in the config will be regex matched against the device name
+    skip_gpus = []
+    if "noPassthroughGpus" in config:
+        skip_gpus = config["noPassthroughGpus"]
+
+    requires_configuration = False
+    has_non_passthrough = False
+
+    for device in devices:
+        skip = False
+
+        if "driver" not in device:
+            continue
+
+        for skip_gpu in [item for item in skip_gpus if "device" in device]:
+            skip = skip_gpu.lower() in device["device"].lower()
+            if skip:
+                break
+
         if skip:
-            break
-
-    if skip:
-        # Check if the device is already set up for passthrough
-        if device["driver"] != "nvidia":
-            requires_configuration = True
-        
-        has_non_passthrough = True
-        continue
+            # Check if the device is already set up for passthrough
+            if device["driver"] != "nvidia":
+                requires_configuration = True
+            
+            has_non_passthrough = True
+            continue
 
 
-    if "NVIDIA" in device["vendor"]:
-        if device["driver"] != "vfio-pci":
-            requires_configuration = True
+        if "NVIDIA" in device["vendor"]:
+            if device["driver"] != "vfio-pci":
+                requires_configuration = True
 
-        bus_ids.append(device["slot"])
-        device_ids.add(device["vendor_id"] + ":" + device["device_id"])
+            bus_ids.append(device["slot"])
+            device_ids.add(device["vendor_id"] + ":" + device["device_id"])
 
-output({
-    "bus_ids": " ".join(bus_ids),
-    "device_ids": ",".join(device_ids),
-    "has_non_passthrough": has_non_passthrough,
-    "requires_configuration": requires_configuration
-})
+    output({
+        "bus_ids": " ".join(bus_ids),
+        "device_ids": ",".join(device_ids),
+        "has_non_passthrough": has_non_passthrough,
+        "requires_configuration": requires_configuration
+    })
+
+if __name__ == "__main__":
+    try:
+        main() 
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        exit(1)
+    except KeyboardInterrupt:
+        print("Interrupted by user", file=sys.stderr)
+        exit(1)
