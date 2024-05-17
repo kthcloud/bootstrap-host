47c75bdc91336cfaa6c896511d14253167627abc
Harald Barth <haba@kth.se>
2024-05-17 15:56:35 +0200

    remove whitespace and generate cloudinit netplan output as well

--- addexternalif.sh
+++ addexternalif.sh
@@ -1,4 +1,4 @@
-#!/bin/bash                                                                                                    
+#!/bin/bash
 IF=`ip route | awk '$1 ~ "172.30.0.0/24" {print $3}'`
 if test $IF ; then
     : all well
@@ -8,11 +8,14 @@ fi
 CLOUDINIT=/etc/cloud/cloud.cfg.d/50-curtin-networking.cfg
 
 cp -p "$CLOUDINIT" "$CLOUDINIT".`date +%s`
-cat << EOF >> "$CLOUDINIT"                                                                                     
-  vlans:                                                                                                       
-    vlan.18:                                                                                                   
-      id: 18                                                                                                   
-      link: $IF                                                                                                
-      mtu: 1500                                                                                                
+cat << EOF >> "$CLOUDINIT"
+  vlans:
+    vlan.18:
+      id: 18
+      mtu: 1500
+      link: $IF
 EOF
+cat "$CLOUDINIT" | sed -e 's/^\( *\)/\1\1/g' > /etc/netplan/50-cloud-init.yaml
+netplan generate
+netplan apply
 exit 0
