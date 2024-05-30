bc67e82287112be8e066790b64a9f8692cf13ee3
Harald Barth <haba@kth.se>
2024-05-30 13:33:55 +0200

    Add rp_filter config to be able to do metallb balancing on external L2 network

--- addexternalif.sh
+++ addexternalif.sh
@@ -3,7 +3,8 @@ IF=`ip route | awk '$1 ~ "172.30.0.0/24" {print $3}'`
 if test $IF ; then
     : all well
 else
-    echo Interface not found
+    echo "External interface not found (only error in Kista)"
+    exit 0 # do not fail the install
 fi
 CLOUDINIT=/etc/cloud/cloud.cfg.d/50-curtin-networking.cfg
 
@@ -18,4 +19,15 @@ EOF
 cat "$CLOUDINIT" | sed -e 's/^\( *\)/\1\1/g' > /etc/netplan/50-cloud-init.yaml
 netplan generate
 netplan apply
+
+SYSCONFFILE=/etc/sysctl.d/10-network-security.conf
+if test -f "$SYSCONFFILE" ; then
+    # patch boot config
+    sed -i 's/rp_filter=2/rp_filter=0/g' "$SYSCONFFILE"
+    # patch running config
+    for f in /proc/sys/net/ipv4/conf/*/rp_filter ; do echo 0 > $f; done
+else
+    echo "Warning: $SYSCONNFILE not found"
+fi
+
 exit 0
